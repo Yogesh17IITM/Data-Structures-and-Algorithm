6884a605e0ac94dac3b7de01bce7fd1db44a261d
Yogesh17IITM <yogeshwaranrubin@gmail.com>
2022-07-03 19:37:44 +0530

    Update Prog_5_NonLinearDS_1_Graph_DFS_Example.cpp
    Created graph class and modified logic accordingly

--- Prog_5_NonLinearDS_1_Graph_DFS_Example.cpp
+++ Prog_5_NonLinearDS_1_Graph_DFS_Example.cpp
@@ -3,84 +3,120 @@ Demonstration of Non-linear data structure (Graph)
 
 Sum of the minimum elements in all connected components of an undirected graph
 
-	    [4]			[2]    [3]
-	   /  \
-	 [1]   [5]
+        [4]			[2]    [3]
+       /  \
+     [1]   [5]
 
 Index:	1	2	3	4	5
 Value:	4	5	3	2	8
 
 Input:
-	No. of Connected Pairs = 2
-	1) [1] - [4]
-	2) [4] - [5]
+    No. of Connected Pairs = 2
+    1) [1] - [4]
+    2) [4] - [5]
 
 Output:
-	Sum of Min elem of connect pairs: 
-	1) Min(Val[1], Val[4], Val[5]) = Min(4, 2, 8) = 2	(since 3 nodes interconnected)
-	2) Val[2] = 5 (only node --> Which will be considered as Min)
-	3) Val[3] = 3 (same as above)
+    Sum of Min elem of connect pairs:
+    1) Min(Val[1], Val[4], Val[5]) = Min(4, 2, 8) = 2	(since 3 nodes interconnected)
+    2) Val[2] = 5 (only node --> Which will be considered as Min)
+    3) Val[3] = 3 (same as above)
 
-	Sum = 2 + 5 + 3 = 10	
+    Sum = 2 + 5 + 3 = 10
 */
-#include<iostream>
-#include<algorithm>
-#include<vector>
+
+#include <iostream>
+#include <vector>
 using namespace std;
 
-const int NMax = 10000;
-vector<int> vecPair[NMax];	// Array Of Vector containing Pair Details
-bool bIsVisited[NMax];		// Flag to identify if the node is visited already
-
-// Connect Nodes through Edges
-void ConnectPair(int u, int v)
+class Graph
 {
-	vecPair[u - 1].push_back(v-1);
-	vecPair[v - 1].push_back(u-1);
+    int _NbNodes;
+    vector<int> *_pAdj = nullptr;
+    bool *_bIsVisited = nullptr;
+
+    void DFS(int &ioMinElem, int iArr[], int iCurrIdx);
+
+public:
+    Graph(int iNbNodes);
+    ~Graph();
+
+    void ConnectNodes(int u, int v);
+    int SumOfMinElem(int iArr[], int nArr);
+};
+
+Graph::Graph(int iNbNodes) : _NbNodes(iNbNodes)
+{
+    _pAdj = new vector<int>[_NbNodes];
+    _bIsVisited = new bool[_NbNodes];
+    for (int idx = 0; idx < _NbNodes; idx++)
+        _bIsVisited[idx] = false;
 }
 
-// Depth First Search (Recursive)
-// Visit all nodes, then update flag and minimum value
-void DFS(int iNodeIdx, int iArr[], int & ioMin)
-{	
-	ioMin = min(iArr[iNodeIdx], ioMin);
-	bIsVisited[iNodeIdx] = true;
-	for (int idx : vecPair[iNodeIdx])	
-	{
-		if(!bIsVisited[idx])
-			DFS(idx, iArr, ioMin);
-	}
+Graph::~Graph()
+{
+    if (_pAdj)
+        delete[] _pAdj;
+
+    if (_bIsVisited)
+        delete[] _bIsVisited;
 }
 
-// Find Sum of minimum element of all connected Components or Nodes
-int SumOfMinimum(int iArr[], int iSize)
+void Graph::ConnectNodes(int u, int v)
 {
-	int oSum = 0;
+    _pAdj[u - 1].push_back(v - 1); // since given idx: starts from 1, not 0
+    _pAdj[v - 1].push_back(u - 1);
+}
 
-	for (int idx = 0; idx < iSize; idx++)
-	{
-		if (!bIsVisited[idx])
-		{
-			int minimum = iArr[idx];	
-			DFS(idx, iArr, minimum);
-			oSum += minimum;
-		}
-	}
-	return oSum;
+void Graph::DFS(int &ioMinElem, int iArr[], int iCurrIdx)
+{
+    // Comare and consider only the minimum value (NOT INDEX)
+    ioMinElem = min(ioMinElem, iArr[iCurrIdx]);
+
+    for (auto &iAdj : _pAdj[iCurrIdx])
+    {
+        // Check if the adjacent idx is not visited yet
+        if (!_bIsVisited[iAdj])
+        {
+            // Update the visited flag for adjacent node
+            _bIsVisited[iAdj] = true;
+
+            // Call recursively for each adjacent node
+            DFS(ioMinElem, iArr, iAdj);
+        }
+    }
+}
+
+int Graph::SumOfMinElem(int iArr[], int nArr)
+{
+    int oSum = 0;
+    for (int idx = 0; idx < nArr; idx++)
+    {
+        // Ensure if the current index is not visited yet
+        if (!_bIsVisited[idx])
+        {
+            // Update the visited flag for current node
+            _bIsVisited[idx] = true;
+
+            int minElem = iArr[idx];
+            DFS(minElem, iArr, idx); // for traversing to the connecting nodes
+
+            // Add-up the result
+            oSum += minElem;
+        }
+    }
+    return oSum;
 }
 
 int main()
 {
-	int arr[] = { 4, 5, 3, 2, 8 };
-	int ArrSize = sizeof(arr) / sizeof(arr[0]);
-	int nPair = 2;
+    int arr[] = {4, 5, 3, 2, 8};
+    int nArr = sizeof(arr) / sizeof(arr[0]);
 
-	// Connect pairs through edges
-	ConnectPair(1, 4);
-	ConnectPair(4, 5);		
+    Graph g(nArr);
+    g.ConnectNodes(1, 4);
+    g.ConnectNodes(4, 5);
 
-	// Display Sum
-	cout << SumOfMinimum(arr, ArrSize) << endl;
+    cout << "Sum of Min. elem: " << g.SumOfMinElem(arr, nArr) << endl;
 
-	return 0;
+    return 0;
 }
