36651c1460457cf87ac4e53c7685b89671cdafbb
Yogesh17IITM <yogeshwaranrubin@gmail.com>
2022-07-10 12:28:00 +0530

    Update Prog_1_Sorting_1_BubbleSort.cpp
    Updated comments

--- Prog_1_Sorting_1_BubbleSort.cpp
+++ Prog_1_Sorting_1_BubbleSort.cpp
@@ -1,11 +1,11 @@
 /*
 Topic : Sorting
 
-Objective: 
+Objective:
     To demonstrate Bubble sort algorithm
 
 Key Topics:
-    - Bubble sort
+    - Bubble sort [Efficient way]
     - swapping variables
 */
 
@@ -14,15 +14,15 @@ Key Topics:
 using namespace std;
 
 void Sort_using_BubbleSort(int iarr[], int iSize);
-void swap(int * ipA, int * ipB);
+void swap(int *ipA, int *ipB);
 void PrintArray(int iArr[], int iSize);
 
 int main()
 {
-    int ListOfInts[]{3,56,1,44,54,45};
-    
-    // sort     
-    int nSize = sizeof(ListOfInts)/sizeof(int);
+    int ListOfInts[]{3, 56, 1, 44, 54, 45};
+
+    // sort
+    int nSize = sizeof(ListOfInts) / sizeof(int);
     Sort_using_BubbleSort(ListOfInts, nSize);
 
     // display
@@ -31,31 +31,37 @@ int main()
 
 void Sort_using_BubbleSort(int iarr[], int iSize)
 {
-    for(int idx=0; idx<(iSize-1); idx++)
+    // Iterate over No. of Pairs
+    // Here No. of pairs = (iSize - 1)
+    for (int idx = 0; idx < (iSize - 1); idx++)
     {
+        // Flag to check if there is any swapping happened [to increase efficiency]
         bool IsSwapped = false;
-        for(int jdx=0; jdx<(iSize-idx-1); jdx++)
+        for (int jdx = 0; jdx < (iSize - idx - 1); jdx++)
         {
-            if (iarr[jdx] > iarr[jdx+1])
+            if (iarr[jdx] > iarr[jdx + 1])
             {
                 // swap the data
-                swap(&iarr[jdx], &iarr[jdx+1]);
+                swap(&iarr[jdx], &iarr[jdx + 1]);
                 IsSwapped = true;
-            }            
+            }
         }
 
+        // If no swapping happened for one full iteration, then it is Sorted
+        // Hence, break the loop
         if (!IsSwapped)
             break;
     }
 }
 
-void swap(int * ipA, int * ipB)
+void swap(int *ipA, int *ipB)
 {
+    // Swapping without using any extra variable
     if (ipA && ipB)
     {
-        *ipA = *ipA + *ipB;
-        *ipB = *ipA - *ipB;
-        *ipA = *ipA - *ipB;
+        *ipA = *ipA + *ipB; // a = a+b
+        *ipB = *ipA - *ipB; // b = a-b  // Now 'b' will contain 'a' value
+        *ipA = *ipA - *ipB; // a = a-b  // Now 'a' will contain 'b' value
     }
 }
 
@@ -63,6 +69,6 @@ void PrintArray(int iArr[], int iSize)
 {
     for (int i = 0; i < iSize; i++)
     {
-        cout<<"val: "<<iArr[i]<<endl;
-    }    
+        cout << "val: " << iArr[i] << endl;
+    }
 }
